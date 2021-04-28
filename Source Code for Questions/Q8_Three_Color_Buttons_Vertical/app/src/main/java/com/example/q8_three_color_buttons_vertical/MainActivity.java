0629bd076a5959beda1aaa2e8e9147dc84f7d2f5
Jatin Kamboj <47236394+MJK618@users.noreply.github.com>
2021-04-28 22:07:15 +0530

    Adding Question 8 source code

---
+++ Source Code for Questions/Q8_Three_Color_Buttons_Vertical/app/src/main/java/com/example/q8_three_color_buttons_vertical/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.q8_three_color_buttons_vertical;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    View view;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        view = this.getWindow().getDecorView();
+        view.setBackgroundResource(R.color.gray);
+    }
+    public void goRed(View v) {
+        view.setBackgroundResource(R.color.red);
+    }
+    public void goBlue(View v) {
+        view.setBackgroundResource(R.color.blue);
+    }
+    public void goGreen(View v) {
+        view.setBackgroundResource(R.color.green);
+    }
+}
